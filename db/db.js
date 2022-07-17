92087d33b00a6195fd053a7d3ca8dcb5905240a8
Jessica Turner <jmcclung3509@gmail.com>
2022-07-16 21:06:00 -0700

    Changed database

--- db/db.js
+++ db/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 module.exports.bootstrap = () => {
     var connectionString = process.env.MONGO_URI;
     mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
